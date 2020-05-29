3b3ed142c78aa8867c203bc26c0ac1599d81b220
vergarapog <201801166@iacademy.edu.ph>
2020-05-30 07:00:15 +0800

    regis login part 2

--- php-scripts/signup.script.php
+++ php-scripts/signup.script.php
@@ -3,4 +3,13 @@
 if (isset($_POST['signup-submit'])) {
 
   require 'databasehandler.script.php';
+
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confpassword = $_POST['confpassword'];
+
+   if (empty($username) || empty($email) || empty($password) || empty($confpassword)) {
+     header("Location: ../signup.php?error=emptyfields&username=".$username."&email=".$email);
+   }
 }
