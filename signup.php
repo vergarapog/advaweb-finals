b1deea9de506b01606336c45ded069ca8605a9d4
vergarapog <201801166@iacademy.edu.ph>
2020-05-30 22:05:46 +0800

    login regis modified - no design

--- signup.php
+++ signup.php
@@ -6,6 +6,19 @@
     <div >
       <section>
         <h1>Registration Page</h1>
+        <?php
+          if(isset($_GET['error'])){
+            if($_GET['error'] == "emptyfields"){
+              echo '<p class = "">Fill in all fields!</p>';
+            }
+            else if ($_GET['error'] == "emptyfields"){
+              echo '<p class = "">Fill in all fields!</p>';
+            }
+          }
+          else if ($_GET["signup"] == "success"){
+            echo '<p class = "">Sign up Successful!</p>';
+          }
+         ?>
         <form action="php-scripts/signup.script.php" method="post">
           <input type="text" name="username" placeholder="Username" >
           <input type="text" name="email" placeholder="E-mail" >
