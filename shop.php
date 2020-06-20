a7b8e9b6aad121d70838b88bdc3439b25077c7de
chris4b7 <201801139@iacademy.ph>
2020-06-20 11:29:24 +0800

    shop fixed part 2

--- shop.php
+++ shop.php
@@ -21,159 +21,25 @@
 <body>
 
 <!-- DIV AREA PART 2 START border =========================================================================================== -->
-   <div id="top"><!-- DIV 1 START -->
-
-       <div class="container"><!-- DIV CONTAINER START -->
-
-           <div class="col-md-6 offer"><!-- DIV 2 START -->
-
-               <a href="#" class="btn btn-success btn-sm">Welcome</a>
-               <a href="checkout.php">Template: 4 Items In Your Cart | Total Price: Php 300 </a>
-
-           </div><!-- DIV 2 END -->
-
-           <div class="col-md-6"><!-- DIV 3 START -->
-
-               <ul class="menu"><!-- MENU START -->
-
-                   <li>
-                       <a href="signup.php">Register</a>
-                   </li>
-                   <li>
-                       <a href="checkout.php">My Account</a>
-                   </li>
-                   <li>
-                       <a href="cart.php">Go To Cart</a>
-                   </li>
-                   <li>
-                       <a href="Login.php">Login</a>
-                   </li>
-
-               </ul><!-- MENU END -->
-
-           </div><!-- DIV 3 END -->
-
-       </div><!-- DIV CONTAINER END -->
-
-   </div><!-- DIV 1 END -->
-
-<!-- DIV AREA PART 1 END border =========================================================================================== -->
-
-
-<!-- DIV AREA PART 2 START border =========================================================================================== -->
-
-   <div id="navbar" class="navbar navbar-default">
+   <!--<div id="top">
 
        <div class="container">
 
-           <div class="navbar-header">
-
-               <a href="index.php" class="navbar-brand home">
-
-                   <img src="images/" alt="Add to cart" class="hidden-xs">
-                   <img src="images/" alt="Add to cart" class="visible-xs">
-
-               </a>
-
-               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
-
-                   <span class="sr-only">Toggle Navigation</span>
-
-                   <i class="fa fa-align-justify"></i>
-
-               </button>
-
-               <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
-
-                   <span class="sr-only">Toggle Search</span>
-
-                   <i class="fa fa-search"></i>
-
-               </button>
-
-           </div>
-
-           <div class="navbar-collapse collapse" id="navigation">
-
-               <div class="padding-nav">
-
-                   <ul class="nav navbar-nav left">
-
-                       <li>
-                           <a href="index.php">Home</a>
-                       </li>
-                       <li class="active">
-                           <a href="shop.php">Shop</a>
-                       </li>
-                       <li>
-                           <a href=" ">My Account</a>
-                       </li>
-                       <li>
-                           <a href=" ">Shopping Cart</a>
-                       </li>
-                       <li>
-                           <a href=" ">Contact Us</a>
-                       </li>
-
-                   </ul>
-
-               </div>
-
-               <a href=" " class="btn navbar-btn btn-primary right">
-
-                   <i class="fa fa-shopping-cart"></i>
-
-                   <span>4 Items In Your Cart</span>
-
-               </a>
-
-               <div class="navbar-collapse collapse right">
-
-                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search">
-
-                       <span class="sr-only">Toggle Search</span>
-
-                       <i class="fa fa-search"></i>
-
-                   </button>
-
-               </div>
-
-               <div class="collapse clearfix" id="search">
-
-                   <form method="get" action="results.php" class="navbar-form">
-
-                       <div class="input-group">
-
-                           <input type="text" class="form-control" placeholder="Search" name="user_query" required>
-
-                           <span class="input-group-btn">
-
-                           <button type="submit" name="search" value="Search" class="btn btn-primary">
-
-                               <i class="fa fa-search"></i>
-
-                           </button>
-
-                           </span>
-
-                       </div>
-
-                   </form>
-
-               </div>
-
+           <div class="col-md-6 offer">
+               <a href="#" class="btn btn-success btn-sm">Welcome</a>
+               <a href="checkout.php">Template: 4 Items In Your Cart | Total Price: Php 300 </a>
            </div>
 
        </div>
 
-   </div>
-
+   </div> -->
 
 <!-- DIV AREA PART 2 END order =========================================================================================== -->
 
 
 <!-- DIV AREA PART 3 START border =========================================================================================== -->
+  <br>
+  <br>
 
    <div id="content">
        <div class="container">
@@ -631,11 +497,11 @@
 
 
    <!-- Caller of the footer-->
-   <!-- <?php
+   <?php
 
     include("view-comp/footer.php");
 
-    ?> -->
+    ?>
 
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
