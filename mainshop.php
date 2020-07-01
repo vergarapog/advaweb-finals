7b05831f11ebed36fbd9aed61894cfa1eaa584d3
vergarapog <201801166@iacademy.edu.ph>
2020-07-01 16:00:27 +0800

    fixed shop bug

--- mainshop.php
+++ mainshop.php
@@ -17,7 +17,7 @@
 
  			 $dynamicList .= '<table width="100%" border="0" cellspacing="0" cellpadding="6" class="center">
          <tr>
-           <td width="17%" valign="top"><a href="product.php?id=' . $id . '"><img style="border:#666 20px solid;" src="images/' . $id . '.png" alt="' . $product_name . '" width="600" height="400" border="1" /></a></td>
+           <td width="17%" valign="top"><a href="product.php?id=' . $id . '"><img style="border:#666 15px solid;" src="images/' . $id . '.png" alt="' . $product_name . '" width="600" height="400" border="1" /></a></td>
            <td width="83%" valign="top" style="color:white;"><b>' . $product_name . '</b><br />
              ‎₱' . $price . '<br /><br><br>
              <a class="p-3 mb-2 bg-info text-white" href="product.php?id=' . $id . '">View Product Details</a></td>
@@ -34,21 +34,7 @@
      <p><?php echo $dynamicList; ?><br />
        </p>
 
-       <div id="grid">
-  <div id="a">a</div>
-  <div id="b">b</div>
-  <div id="c">c</div>
-  <div id="d">d</div>
-  <div id="e">e</div>
-  <div id="f">f</div>
-  <div id="g">g</div>
-  <div id="h">h</div>
-</div>
-
-
-
-  </main>
-
+     </main>
 
 
 
